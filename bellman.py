e5e29570ab57cc290d7e681ff4290eb339c1f97e
valitovsen <valitov.arseny@gmail.com>
2017-12-09 16:35:42 +0300

    minor changes

--- bellman.py
+++ bellman.py
@@ -36,9 +36,9 @@ def bellman(graph, source):
         dyn_array[i] = {}
         for end in node_list:
             opt1 = dyn_array[i-1][end] # shortest path under previous node limit
-            opt2 = min([dyn_array[i-1][start]+graph[start][end] # shortest path under current node limit
-                            for start in reversed_edge_list.get(end,[])])
-            dyn_array[i][end] = min(opt1,opt2) # pick minimum
+            opt2 = [dyn_array[i-1][start]+graph[start][end] # shortest path under current node limit
+                            for start in reversed_edge_list.get(end,[])]
+            dyn_array[i][end] = min(opt1+opt2) # pick minimum
             comp = comp & (dyn_array[i-1][end] == dyn_array[i][end]) # check progress, False if paths were changed
         del dyn_array[i-1] # memory optimization, won't need it in further steps
 
